ad4ec7fff34a9de688a44504b8316ffbc810e883
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2019-02-01 17:47:22 -0500

    correcting logic

--- developments_build/sql/x_outlier.sql
+++ developments_build/sql/x_outlier.sql
@@ -2,8 +2,14 @@ UPDATE developments
 SET x_outlier = TRUE
 WHERE job_number IN
 	(SELECT DISTINCT job_number
-		FROM qc_outliers
-		WHERE outlier IS DISTINCT FROM 'N' OR outlier IS DISTINCT FROM 'C');
-
+		FROM qc_outliers 
+			WHERE job_number NOT IN 
+			(SELECT DISTINCT job_number 
+				FROM qc_outliersacrhived 
+				WHERE outlier = 'N' OR outlier = 'C') 
+			AND job_number NOT IN 
+			(SELECT DISTINCT job_number 
+				FROM developments 
+				WHERE x_dcpedited <> 'true'));
 -- Remove the data table
 DROP TABLE IF EXISTS qc_outliers;
