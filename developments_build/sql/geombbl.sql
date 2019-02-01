8899ed136ae7371e622aacf247660d88ab12be8b
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2019-02-01 18:45:08 -0500

    adding logic to improve geocoding

--- developments_build/sql/geombbl.sql
+++ developments_build/sql/geombbl.sql
@@ -1,9 +1,16 @@
--- set the geometry to be the center of the lot
+-- set the geometry to be the center of the lot using lat/long
+UPDATE developments a
+SET geom = ST_SetSRID(ST_MakePoint(a.longitude::double precision, a.latitude::double precision),4326),
+	x_geomsource = 'Lat/Long geosupport'
+WHERE a.geom IS NULL
+AND a.longitude IS NOT NULL AND a.geo_bbl IS NOT NULL;
+-- set the geometry to be the center of the lot using mappluto
 UPDATE developments a
 SET geom = ST_Centroid(b.geom),
 	x_geomsource = 'BBL geosupport'
 FROM pluto b
 WHERE a.geo_bbl::text = b.bbl::text
+AND a.geom IS NULL
 AND b.geom IS NOT NULL;
 
 UPDATE developments a
--- developments_build/sql/latlong.sql
+++ developments_build/sql/latlong.sql
@@ -2,8 +2,10 @@
 UPDATE developments
 SET latitude = ST_Y(geom),
 	longitude = ST_X(geom)
-	WHERE geom IS NOT NULL;
+	WHERE geom IS NOT NULL
+	AND x_geomsource <> 'Lat/Long geosupport';
 -- If there is no geometry but there is a lat / long then create the geometry
 UPDATE developments
-SET geom = ST_SetSRID(ST_MakePoint(longitude::double precision, latitude::double precision), 4326)
+SET geom = ST_SetSRID(ST_MakePoint(longitude::double precision, latitude::double precision), 4326),
+	x_geomsource = 'Lat/Long DOB'
 WHERE geom IS NULL AND longitude IS NOT NULL;
