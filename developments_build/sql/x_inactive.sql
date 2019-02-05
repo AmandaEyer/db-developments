70cd90654b482f663c952d209b1bc35d85024492
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2019-02-05 16:36:10 -0500

    flagging records where one is not complete but an associated job is complete as inactive

--- developments_build/sql/x_inactive.sql
+++ developments_build/sql/x_inactive.sql
@@ -15,6 +15,29 @@ UPDATE developments
 	WHERE status = 'In progress'
 	AND (CURRENT_DATE - status_date::date)/365 >= 3;
 
+-- Tag projects that are withdrawn
+UPDATE developments
+	SET x_inactive = TRUE
+	WHERE status = 'Withdrawn';
+
+-- Tag projects that are potential duplicates 
+-- and the status date is older than an associated complete record
+WITH completejobs AS (
+	SELECT address, job_type, status_date, status
+	FROM developments
+	WHERE units_net::numeric > 0
+	AND status LIKE 'Complete%')
+UPDATE developments a 
+SET x_inactive = TRUE
+FROM completejobs b
+WHERE a.address = b.address
+	AND a.job_type = b.job_type
+	AND a.status NOT LIKE 'Complete%'
+	AND a.status_date::date < b.status_date::date
+	AND a.status <> 'Withdrawn'
+  	AND a.occ_prop <> 'Garage/Miscellaneous';
+  	
+-- set NULL records to false
 UPDATE developments
 	SET x_inactive = FALSE
 	WHERE x_inactive IS NULL;
