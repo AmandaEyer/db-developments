5f1db95c8893ac4f81130096dff14bbff3545d30
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2019-02-06 11:49:57 -0500

    removing units_hotels.sql and adding code into units.sql

--- developments_build/sql/units_.sql
+++ developments_build/sql/units_.sql
@@ -10,6 +10,26 @@ SET units_prop = '0'
 WHERE units_prop IS NULL
 AND job_type = 'Demolition';
 
+-- set units_prop to 0 when multiple dwelling units are coverted to hotels
+-- to reflect the loss of residential units when multiple dwellings are converted into hotels (but only for alterations)
+UPDATE developments
+SET units_prop = '0'
+WHERE job_type = 'Alteration' 
+	AND (occ_init LIKE '%Residential%'
+		OR occ_init LIKE '%Assisted%')
+	AND occ_prop LIKE '%Hotel%'
+	AND x_mixeduse IS NULL;
+
+-- set units_init to 0 when hotels are converted into multiple dwellings
+-- to reflect the gain of residential units when hotels are converted into multiple dwellings (but only for alterations)
+UPDATE developments
+SET units_init = '0'
+WHERE job_type = 'Alteration' 
+	AND occ_init LIKE '%Hotel%'
+	AND (occ_prop LIKE '%Residential%'
+		OR occ_prop LIKE '%Assisted%')
+	AND x_mixeduse IS NULL;
+
 -- populate units_net to capture proposed net change in units
 -- negative for demolitions, proposed for new buildings, and net change for alterations
 -- (note: if an alteration is missing value for existing or proposed units, value set to null)
